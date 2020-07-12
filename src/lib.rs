934edfe608b6390636fe99126eff96b899304f0b
James Hagans <jhagans@acm.org>
2020-07-12 12:02:38 -0400

    set up for gradient square

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use web_sys::WebGlRenderingContext as GL;
+use web_sys::*;
+
+#[macro_use]
+extern crate lazy_static;
+
+mod app_state;
+mod common_funcs;
+mod constants;
+mod gl_setup;
+mod programs;
+mod shaders;
+mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -183,5 +194,57 @@ impl ExampleStruct {
 
     pub fn transfer_ownership(self) -> ExampleStruct {
         self
+    }
+}
+
+#[wasm_bindgen]
+pub struct MyClient {
+    gl: WebGlRenderingContext,
+    program_color_2d: programs::Color2D,
+    program_color_2d_gradient: programs::Color2DGradient,
+}
+
+#[wasm_bindgen]
+impl MyClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        console_error_panic_hook::set_once();
+        let gl = gl_setup::initialize_webgl_context().unwrap();
+
+        Self {
+            program_color_2d: programs::Color2D::new(&gl),
+            program_color_2d_gradient: programs::Color2DGradient::new(&gl),
+            gl: gl,
+        }
+    }
+
+    pub fn update(&mut self, time: f32, height: f32, width: f32) -> Result<(), JsValue> {
+        app_state::update_dynamic_data(time, height, width);
+        Ok(())
+    }
+
+    pub fn render(&self) {
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+        let curr_state = app_state::get_curr_state();
+
+        self.program_color_2d.render(
+            &self.gl,
+            curr_state.control_bottom,
+            curr_state.control_top,
+            curr_state.control_left,
+            curr_state.control_right,
+            curr_state.canvas_height,
+            curr_state.canvas_width,
+        );
+
+        self.program_color_2d_gradient.render(
+            &self.gl,
+            curr_state.control_bottom + 20.,
+            curr_state.control_top - 20.,
+            curr_state.control_left + 20.,
+            curr_state.control_right - 20.,
+            curr_state.canvas_height,
+            curr_state.canvas_width,
+        );
     }
 }
---
+++ src/programs/mod.rs
@@ -0,0 +1,8 @@
+mod graph_3d;
+pub use graph_3d::*;
+
+mod color_2d;
+pub use color_2d::*;
+
+mod color_2d_gradient;
+pub use color_2d_gradient::*;
